6e19b813fc20e855a30977d7b933f03e4685f774
Marc Steele <steelegbr@gmail.com>
2025-11-14 19:54:39 +0000

    Initial Commit

---
+++ OBSMagic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OBSMagic
+//
+//  Created by Marc Steele on 14/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OBSMagic/OBSMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBSMagicApp.swift
+//  OBSMagic
+//
+//  Created by Marc Steele on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct OBSMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
